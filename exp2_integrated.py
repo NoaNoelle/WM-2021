b891a5524ee2edb768bb3018541532a32910c6d6
N.N. Schwensfeier <s3344282@turing10.housing.rug.nl>
2021-09-02 09:42:42 +0200

    initial commit

--- exp2_integrated.py
+++ exp2_integrated.py
@@ -18,17 +18,7 @@ from sklearn.decomposition import PCA
 import time
 
 """
-Integrated Model 1: activity-silent model with content-specific 
-oscillations (alpha) to maintain information in one population and 
-unspecific oscillatory activation (alpha) to delete the information 
-maintained in the primary module following the first probe.
-
-Memory population contains STP neurons, the recurrent connection on the 
-memory population contains the STP learning rule and weights solver. 
-
-Content-specific reactivation is achieved through the sensory maintenance
-mechanism on the primary sensory population; the generic reactivation is 
-fed to the memory population of the primary module after the first probe.
+Integrated Model: CSR onset?
 """
 
 #SIMULATION CONTROL for GUI
